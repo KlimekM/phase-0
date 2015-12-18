40a7ea4dce4badd3743c56c0c957532216341e07
KlimekM <mklimek15@gmail.com>
2015-12-17 21:12:01 -0600

    Update refactored solutions in 6.5

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -1,13 +1,14 @@
 # RELEASE 2: NESTED STRUCTURE GOLF
 # Hole 1
 # Target element: "FORE"
+# Paired with Gary Wong for 1.25 hr
 
 array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
-# attempts:
+# attempts:1
 # ============================================================
 p array[1][1][2][0]
-# puts array.any? {|element| element.any?{|array| array.include?("FORE")}}
+p array.flatten[5]
 
 # ============================================================
 
@@ -16,9 +17,11 @@ p array[1][1][2][0]
 
 hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
-# attempts:
+# attempts:1
 # ============================================================
 p hash[:outer][:inner]["almost"][3]
+p hash.flatten[1].flatten[1].flatten[1].flatten[1]
+
 
 
 # ============================================================
@@ -29,7 +32,7 @@ p hash[:outer][:inner]["almost"][3]
 
 nested_data = {array: ["array", {hash: "finished"}]}
 
-# attempts:
+# attempts:1
 # ============================================================
 p nested_data[:array][1][:hash]
 
@@ -39,34 +42,37 @@ p nested_data[:array][1][:hash]
 # RELEASE 3: ITERATE OVER NESTED STRUCTURES
 
 number_array = [5, [10, 15], [20,25,30], 35]
-
 number_array.each do |element|
   if element.kind_of? (Array)
     element.map{|num| p num + 5}
-  end
-  if element.kind_of? (Integer)
-   p element + 5
+  elsif element.kind_of? (Integer)
+    p element + 5
   end
 end
 
-# Attempted to refactor to a shorter solution, but were not having success in doing so.
+# /// Refactor
+p number_array.flatten.each{|x| x+5}
+
 
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
-startup_names.each do |outer_array|
-  if outer_array.kind_of? (Array)
-    outer_array.map do |inner_array|
-        if inner_array.kind_of? (Array)
-          inner_array.map {|inner_string| p inner_string + "ly"}
-        else
-          p inner_array + "ly"
-        end
-    end
-  else
-    p outer_array + "ly"
-  end
-end
+# startup_names.each do |outter_array|
+#   if outter_array.kind_of? (Array)
+#     outter_array.map do |inner_array|
+#         if inner_array.kind_of? (Array)
+#           inner_array.map {|inner_string| p inner_string + "ly"}
+#         else
+#           p inner_array + "ly"
+#         end
+#     end
+#   else
+#     p outter_array + "ly"
+#   end
+# end
+
+# /// Refactor
+p startup_names.flatten.map{|word| word + "ly"}
 
 =begin
 What are some general rules you can apply to nested arrays?
@@ -81,5 +87,5 @@ We can iterate over nester arrays using .each and any enumerable method, but we 
 Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
 
 We used the .kind_of? method which looks for an array or integer or whatever we ask it for. In addition to that we used .map which I have used before,
-but got a good refresher on.
+but got a good refresher on. My pair also found the flatten method during refactoring which compresses the nested arrays into a single array.
 =end
